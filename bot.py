be378500908f4e5f4590bc146b9bdf985d16527b
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2019-01-08 04:10:05 +0100

    Reworked things into classes
    got annoying passing the driver around, and I needed more things passed around after a while.

--- bot.py
+++ bot.py
@@ -16,6 +16,44 @@ import random
 
 import traceback
 
+
+class Logger:
+
+    def __init__(self, sys_log="AMQ-bot.log", chat_log="AMQ-bot-chat.log",
+                 chat_out_log="AMQ-bot-chat_out.log",
+                 event_log="AMQ-bot-events.log"
+                 ):
+        self.sys_log = sys_log
+        self.chat_log = chat_log
+        self.chat_out_log = chat_out_log
+        self.event_log = event_log
+
+    def log(self, message, logfile):
+        """
+        logs events to the logfile
+        """
+        # write timestamp message newline to file
+        msg = "[%s]: %s\n" % (
+            datetime.datetime.fromtimestamp(
+                datetime.datetime.now().timestamp()
+            ).isoformat(), message)
+        with open(logfile, "a", encoding="utf-8") as f:
+            f.write(msg)
+        print(msg)
+
+    def sys(self, message):
+        self.log(message, self.sys_log)
+
+    def chat(self, message):
+        self.log(message, self.chat_log)
+
+    def chat_out(self, message):
+        self.log(message, self.chat_out_log)
+
+    def event(self, message):
+        self.log(message, self.event_log)
+
+
 class Player(object):
     """represents a player"""
     def __init__(self, username, note=""):
@@ -62,12 +100,15 @@ class WaitingLobby:
 
 
 class MessageManager:
-    database = {"greeting": 3, "kick_chat": 1, "kick_pm": 1, "banned_word": 1,
-                "log_chat": 1, "something": 1}
+    database = {"greeting_player": 3, "kick_chat": 1, "kick_pm": 1,
+                "banned_word": 1, "permission_denied": 1,
+                "log_chat_out": 1, "something": 1, }
     langpack = "./langs/"
 
     def __init__(self, directory="en_UK"):
         self.directory = directory
+        self.path = self.langpack + directory + "/"
+        # self.database = MessageManager.database
 
     def get_message(self, name, substitutions=[], number=None):
         filename = name
@@ -79,154 +120,256 @@ class MessageManager:
             else:
                 filename += name + str(self.database[name]-1)
         filename += ".txt"
-        with open(langpack + directory + filename) as file:
+        with open(self.path + filename) as file:
             base_text = file.read()
         for n in range(len(substitutions)):
-            base_text = re.sub("$%d" % n+1, substitutions[n], base_text)
+            base_text = re.sub("&%d" % (n+1), substitutions[n], base_text)
         return base_text
 
 
-def log(message):
-    """
-    logs events to the logfile
-    """
-    # write timestamp message newline to file
-    msg = "[%s]: %s\n" % (
-        datetime.datetime.fromtimestamp(
-            datetime.datetime.now().timestamp()
-        ).isoformat(), message)
-    with open(logfile, "a", encoding="utf-8") as f:
-        f.write(msg)
-    print(msg)
-
-
-logfile = "AMQ-bot.log"
-
-
-def chat(driver, message):
-    chatbox = driver.find_element_by_id("gcInput")
-    chatbox.send_keys(message)
-    chatbox.send_keys(Keys.ENTER)
-    log("Sent message: %s" % message)
-
-
-chat_pos = 0
-
-
-def scan_chat(driver):
-    """
-    scans the chat for interesting events.
-    Includes, but is not limited to:
-    chat commands
-    players joining/leaving
-    kicking/banning for unwanted phrases
-    """
-    global chat_pos
-    chat_window = driver.find_element_by_id("gcMessageContainer")
+class Game:
     chat_pattern = re.compile(r"(?s)<li>(.*?)</li>")
-    chat_html = chat_window.get_attribute('innerHTML')
-    chat_messages = chat_pattern.findall(chat_html)
-    # print(chat_window.text)
-    player_message_pattern = re.compile(r'(?s)<img class="backerBadge.*?>.*<span class="gcUserName(?: clickAble" data-original-title="" title=")?">(.*?)</span>(.*)\n')
-    player_joined_as_player = re.compile(r'<span>(.*?) joined the room.</span><br>')
-    player_joined_as_spectator = re.compile(r'<span>(.*?) has started spectating.</span><br>')
-    player_changed_to_player = re.compile(r'<span>(.*?) changed to player</span><br>')
-    player_changed_to_spectator = re.compile(r'<span>(.*?) changed to spectator</span><br>')
-    player_left = re.compile(r'<span>(.*?) has left the game.</span><br>')
-    detect_amq_emoji = re.compile(r'\n\s<img class="amqEmoji" alt="(.*?)" draggable="false" src=".*?">\n')
-    detect_emoji = re.compile(r'<img class="emoji" draggable="false" alt=".*?" src="(.*?)">')
+    player_message_pattern = re.compile(
+        r'(?s)<img class="backerBadge.*?>.*<span class="gcUserName' +
+        r'(?: clickAble" data-original-title="" title=")?">(.*?)</span>(.*)\n')
+    player_joined_as_player = re.compile(
+        r'<span>(.*?) joined the room.</span><br>')
+    player_joined_as_spectator = re.compile(
+        r'<span>(.*?) has started spectating.</span><br>')
+    player_changed_to_player = re.compile(
+        r'<span>(.*?) changed to player</span><br>')
+    player_changed_to_spectator = re.compile(
+        r'<span>(.*?) changed to spectator</span><br>')
+    player_left_pattern = re.compile(
+        r'<span>(.*?) has left the game.</span><br>')
+    detect_amq_emoji = re.compile(
+        r'\n\s<img class="amqEmoji" alt="(.*?)"' +
+        r' draggable="false" src=".*?">\n')
+    detect_emoji = re.compile(
+        r'<img class="emoji" draggable="false" alt=".*?" src="(.*?)">')
 
-    for match in chat_messages[chat_pos:]:
-        print(match)
-        player_message = player_message_pattern.search(match)
-        join_as_player = None
-        join_as_spectator = None
-        spectator_to_player = None
-        player_to_spectator = None
-        player_left = None
-        done = False
-        if player_message:
-            name = player_message.group(1)
-            message = player_message.group(2)
-            num = 1
-            while num == 1:
-                message, num = detect_amq_emoji.subn(r"<\g<1>>", message, 1)
-            num = 1
-            while num == 1:
-                message, num = detect_emoji.subn(r"<\g<1>>", message, 1)
-            print("%s said: \"%s\"" % (name, message))
-            if name not in admins and "bad word" in message:
-                kick_player(driver, name, "Pls do not use such language")
-            elif message[0] == "!":
-                handle_command(driver, name, message[1:])
-            done = True
-        if not done:
-            join_as_player = player_joined_as_player.search(match)
-            # the glory of not being able to assign in an if
-        if not done and join_as_player:
-            done = True
-            pass
-        if not done:
-            join_as_spectator = player_joined_as_spectator.search(match)
-        if not done and join_as_spectator:
-            done = True
-            pass
-        if not done:
-            spectator_to_player = player_changed_to_player.search(match)
-        if not done and spectator_to_player:
-            done = True
-            pass
-        if not done:
-            player_to_spectator = player_changed_to_spectator.search(match)
-        if not done and player_to_spectator:
-            done = True
-            pass
-        if not done:
-            player_left = player_left.search(match)
-        if not done and player_left:
-            done = True
-            pass
-    if len(chat_messages) > chat_pos:
-        chat_pos = len(chat_messages)
-    pass
+    def __init__(self, config_file, admins=[]):
+        self.admins = admins
+        with open(config_file) as f:
+            config = f.read()
+            config = config.split("\n")
+        self.code = config[0]
+        self.geckodriver_path = config[1]
+        self.username = config[2]
+        self.admins.append(self.username)
+        self.password = config[3]
+        self.room_name = config[4]
+        self.room_password = config[5]
+        self.lang = config[6]
+        self.msg_man = MessageManager(directory=self.lang)
+        self.driver = webdriver.Firefox(executable_path=self.geckodriver_path)
+        self.state = 0
+        self.state_timer = 40
+        self.log = Logger()
+        self.chat_pos = 0
 
+    def close(self):
+        self.driver.execute_script("options.logout();")
+        time.sleep(1)
+        self.driver.close()
 
-def message_player(driver, username, message):
-    pass
+    def start(self):
+        self.bootstrap()
+        self.run()
+        pass
 
+    def bootstrap(self):
+        code = self.code
+        username = self.username
+        password = self.password
+        room_name = self.room_name
+        room_password = self.room_password
+        driver = self.driver
+        driver.get('https://animemusicquiz.com')
+        username_input = driver.find_element_by_id("loginUsername")
+        password_input = driver.find_element_by_id("loginPassword")
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        login_button = driver.find_element_by_id("loginButton")
+        login_button.click()
+        time.sleep(2)
+        play = driver.find_element_by_id("mpPlayButton")
+        play.click()
+        time.sleep(2)
+        host = driver.find_element_by_id("roomBrowserHostButton")
+        host.click()
+        time.sleep(2)
+        driver.execute_script("hostModal.selectStandard();")
+        # driver.execute_script("hostModal.selectQD();")
+        time.sleep(2)
+        driver.execute_script("hostModal.toggleLoadContainer();")
+        time.sleep(2)
+        load = driver.find_element_by_id("mhLoadFromSaveCodeButton")
+        load.click()
+        time.sleep(2)
+        load_code_entry = driver.find_element_by_class_name("swal2-input")
+        load_code_entry.send_keys(code)
+        load_confirm = driver.find_element_by_class_name(
+            "swal2-confirm")
+        load_confirm.click()
+        time.sleep(2)
+        if room_password != "":
+            private_button = driver.find_element_by_id("mhPrivateRoom")
+            box = driver.find_element_by_class_name("customCheckbox")
+            actions = ActionChains(driver)
+            actions.move_to_element(box)
+            actions.click(private_button)
+            actions.perform()
+            time.sleep(2)
+            room_password_input = driver.find_element_by_id("mhPasswordInput")
+            room_password_input.send_keys(room_password)
+        room_name_input = driver.find_element_by_id("mhRoomNameInput")
+        room_name_input.send_keys(room_name)
+        start_room = driver.find_element_by_id("mhHostButton")
+        start_room.click()
 
-def kick_player(driver, username, reason=None):
-    reason = reason or "something"
-    driver.execute_script('lobby.kickPlayer("%s")' % username)
-    chat(driver, "Kicked player %s for %s." % (username, reason))
-    message_player(driver, username, "You were kicked for: %s" % reason)
+    def run(self):
+        self.chat("Hello World!")
+        while self.state != -1:
+            if self.state == 0:
+                self.idle()
+            if self.state == 1:
+                pass
+            self.scan_chat()
+            time.sleep(1)
+        self.close()
 
+    def idle(self):
+        self.state_timer -= 1
+        if self.state_timer == 0:
+            self.chat("idle1")
+            self.state_timer = 40
+        self.scan_lobby()
+        if False:
+            self.state = 1
 
-def scan_lobby(driver):
-    pass
+    def chat(self, message):
+        chatbox = self.driver.find_element_by_id("gcInput")
+        chatbox.send_keys(message)
+        chatbox.send_keys(Keys.ENTER)
+        self.log.chat_out(self.msg_man.get_message("log_chat_out", [message]))
 
+    def scan_chat(self):
+        """
+        scans the chat for interesting events.
+        Includes, but is not limited to:
+        chat commands
+        players joining/leaving
+        kicking/banning for unwanted phrases
+        """
+        chat_pos = self.chat_pos
+        chat_window = self.driver.find_element_by_id("gcMessageContainer")
+        chat_pattern = self.chat_pattern
+        chat_html = chat_window.get_attribute('innerHTML')
+        chat_messages = chat_pattern.findall(chat_html)
+        # print(chat_window.text)
+        player_message_pattern = self.player_message_pattern
+        player_joined_as_player = self.player_joined_as_player
+        player_joined_as_spectator = self.player_joined_as_spectator
+        player_changed_to_player = self.player_changed_to_player
+        player_changed_to_spectator = self.player_changed_to_spectator
+        player_left_pattern = self.player_left_pattern
+        detect_amq_emoji = self.detect_amq_emoji
+        detect_emoji = self.detect_emoji
 
-admins = []
-stop = False
+        for match in chat_messages[chat_pos:]:
+            print(match)
+            player_message = player_message_pattern.search(match)
+            join_as_player = None
+            join_as_spectator = None
+            spectator_to_player = None
+            player_to_spectator = None
+            player_left = None
+            done = False
+            if player_message:
+                name = player_message.group(1)
+                message = player_message.group(2)
+                num = 1
+                while num == 1:
+                    message, num = detect_amq_emoji.subn(r"<\g<1>>", message, 1)
+                num = 1
+                while num == 1:
+                    message, num = detect_emoji.subn(r"<\g<1>>", message, 1)
+                self.log.chat("%s said: \"%s\"" % (name, message))
+                if name not in self.admins and "bad word" in message:
+                    self.kick_player(name, self.msg_man.get_message("banned_word"))
+                elif message[0] == "!":
+                    self.handle_command(name, message[1:])
+                done = True
+            if not done:
+                join_as_player = player_joined_as_player.search(match)
+                # the glory of not being able to assign in an if
+            if not done and join_as_player:
+                self.chat(self.msg_man.get_message("greeting_player",
+                                                   [join_as_player.group(1)]))
+                done = True
+                pass
+            if not done:
+                join_as_spectator = player_joined_as_spectator.search(match)
+            if not done and join_as_spectator:
+                done = True
+                pass
+            if not done:
+                spectator_to_player = player_changed_to_player.search(match)
+            if not done and spectator_to_player:
+                done = True
+                pass
+            if not done:
+                player_to_spectator = player_changed_to_spectator.search(match)
+            if not done and player_to_spectator:
+                done = True
+                pass
+            if not done:
+                player_left = player_left_pattern.search(match)
+            if not done and player_left:
+                done = True
+                pass
+        if len(chat_messages) > chat_pos:
+            self.chat_pos = len(chat_messages)
 
+    def message_player(self, username, message):
+        pass
 
-def handle_command(driver, user, command):
-    print("Command detected: %s" % command)
-    if command.lower() == "stop":
-        if user in admins:
-            print("stop detected")
-            global stop
-            stop = True
-        else:
-            chat(driver, "You do not have permission to do that, %s" % user)
-    pass
+    def kick_player(self, username, reason=None):
+        reason = reason or msg_man.get_message("something")
+        self.driver.execute_script('lobby.kickPlayer("%s")' % username)
+        self.chat(self.msg_man.get_message("kick_chat", [username, reason]))
+        self.message_player(username,
+                            self.msg_man.get_message("kick_pm", [reason]))
+
+    def scan_lobby(self):
+        pass
+
+    def handle_command(self, user, command):
+        print("Command detected: %s" % command)
+        if command.lower() == "stop":
+            if user in self.admins:
+                print("stop detected")
+                self.state = -1
+            else:
+                self.chat(self.msg_man.get_message("permission_denied",
+                                                   [user]))
+        pass
 
 
 def main():
     """
     the main function, where the magic happens
     """
-    log("AMQ-automator started")
+    # sys_log("AMQ-automator started")
+    game = Game("default.config")
+    try:
+        game.start()
+    except Exception as e:
+        traceback.print_exc()
+        game.close()
+
+    return
     with open("default.config") as f:
         config = f.read()
         config = config.split("\n")
@@ -297,6 +440,8 @@ def main():
     chat(driver, "Hello World!")
     state = 0  # 0: idle
     state_timer = 40
+    game = Game(driver)
+    game.run()
     try:
         while not stop:
             # program loop
@@ -316,7 +461,7 @@ def main():
     except Exception as e:
         traceback.print_exc()
     else:
-        log("program closed normally")
+        sys_log("program closed normally")
     # input("Press enter to terminate")
     driver.execute_script("options.logout();")
     time.sleep(1)
