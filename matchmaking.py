730ab975a5086ae3821d0a06ed3d087cfc1026f4
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2019-07-31 20:35:29 +0200

    made matchmaking better
    ready for testing

--- matchmaking.py
+++ matchmaking.py
@@ -4,16 +4,19 @@ class Matchmaking:
         self.entries = entries
 
     def add(self, name, rating, desperation=0):
-        if name not in [entry.name for entry in entries]:
-            self.entries.append(MatchMakingEntry(name, rating, desperation))
+        if name not in [entry.name for entry in self.entries]:
+            self.entries.append(MatchmakingEntry(name, rating, desperation))
 
     def remove(self, name):
         self.entries = [entry for entry in self.entries if entry.name != name]
 
     def match(self):
+        if not self.active():
+            return None
         for entry in self.entries:
             result = [entry]
-            for other in self.entries:
+            sorted_list = sorted([other for other in self.entries if other.name != entry.name], key=lambda other: abs(entry.rating-other.rating))
+            for other in sorted_list:
                 if other.name == entry.name:
                     continue
                 if entry.match(other):
@@ -23,8 +26,22 @@ class Matchmaking:
             entry.increase_desperation()
         return None
 
+    def count(self):
+        return len(entries)
 
-class MatchMakingEntry:
+    def active(self):
+        return self.count() >= self.target()
+
+    def __str__(self):
+        ret = "Matchmaking: {\n"
+        count = 0
+        for entry in self.entries:
+            ret += "%d: %s,\n"
+        ret += "}"
+        return ret
+
+
+class MatchmakingEntry:
     def __init__(self, name, rating, desperation=0):
         self.name = name  # name of the entry
         self.rating = rating  # rating of the entry
@@ -37,4 +54,25 @@ class MatchMakingEntry:
         self.desperation = int(self.desperation*1.1) + 1
 
     def __repr__(self):
-        return "MatchMakingEntry('%s', %d, %d)" % (self.name, self.rating, self.desperation)
+        return "MatchmakingEntry('%s', %d, %d)" % (self.name, self.rating, self.desperation)
+
+    def __str__(self):
+        return '{ "name": "%s", "rating": %d, "desperation": %d }' % (self.name, self.rating, self.desperation)
+
+
+if __name__ == "__main__":
+    matchmaking = Matchmaking(int(input("target\n")))
+    while True:
+        name = input("name: ")
+        rating = input("rating: ")
+        if name == "":
+            break
+        matchmaking.add(name, int(rating))
+    while matchmaking.active():
+        print(matchmaking)
+        match = matchmaking.match()
+        if match:
+            print(match)
+            input()
+            for name in match:
+                matchmaking.remove(name)
